48461c625a345aeaef145dccf46f55a0dbd99f00
ronnapatp <ronnapat.sri@gmail.com>
2021-10-15 22:41:25 +0700

    Add about

---
+++ src/components/about.svelte
@@ -0,0 +1,49 @@
+<script>
+
+</script>
+
+<div class="container mx-auto px-2">
+    <h1 class="text-6xl font-bold">
+        About me
+    </h1>
+    <div class="">
+        <p class="text-2xl">
+          Hi everyone ,Thank you for check out mu personal website for my information please continue reading but if you have any question
+          that don't in this page you can email me at <a href="mailto:ronnapat.sri@gmail.com" target="_blank" class="underline hover:text-yellow-600">ronnapat.sri@gmail.com</a>
+        </p>
+        <br>
+        <h3>EDUCATION</h3>
+        <p>
+         <li> Chulalongkorn University Demonstration Elementary School </li>
+         <li> Chulalongkorn University Demonstration Secondary School</li>
+        </p>
+        <br>
+        <h3>TECHNOLOGIES AND LANGUAGES</h3>
+        <p>
+          <li><b>Languages</b> : JavaScript, Node.js, TypeScript, Python, HTML(Storng), CSS </li>
+          <li><b>Frameworks</b> : Astro, ReactJS, NextJS, NestJS, Svelte, Svelte kit, TailwindCSS, Bootstrap, Vue </li>
+          <li><b>Tools</b> : Git, Framework, Windows 10, Powershell, Command prompt </li>
+          <li><b>Other</b> : Firebase, Vercel, Netlify, Godaddy, Github Actions, Github copilot, Github codespace </li>
+        </p>
+        <br>
+        <h3>PROJECTS</h3>
+        <p>
+          <li><b>Kaolad discord bot</b> - Discord bot for Hi i bored community(My friend sever). I helped create bot also build command by using javascript <a href="https://github.com/ronnapatp/kaoladbot" target="_blank">github.com/ronnapatp/kaoladbot</a></li>
+          <li><b>Kumi official website</b> - Website for <a href="https://www.facebook.com/KumiTheChubbyBear" target="_blank" class="underline hover:text-yellow-600">kumi the chubby bear</a>. I helped create react.ts also help build page with tailwind css.</li>
+        </p>
+        <br>
+        <h3>INTERESTS</h3>
+        <p>
+          <li><b>Games</b> : FPS, Offline game, Moba, Puzzle, Game on play station</li>
+          <li><b>Music</b> : EDM, Rock, Musicial</li>
+          <li><b>Pets</b> : I'm looking for have korgi dog</li>
+          <li><b>Social media</b> : Facebook, Instagram, Twitter</li>
+          <li><b>Typing</b> : TH 35wpm ENG 50 wpm</li>
+          <li><b>Keyboard layout</b> : TH <a href="https://manoonchai.com/" target="_blank" class="underline hover:text-yellow-600">Manoonchai</a> EN QWERTY</li>
+        </p>
+        <br>
+        <h3>CONTACT ME</h3>
+        <li><b>Email</b> : <a href="mailto:ronnapat.sri@gmail.com" target="_blank" class="underline hover:text-yellow-600">ronnapat.sri@gmail.com</a></li>
+        <li><b>Twitter</b> : <a href="https://twitter.com/ronnapatp" target="_blank" class="underline hover:text-yellow-600">@ronnapatp</a></li>
+      </div>
+</div>
---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Nav from '../components/navbar.svelte'
+    import About from '../components/about.svelte'
+</script>
+
+
+<Nav />
+<br><br><br><br>
+<About />
