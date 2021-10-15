e1e87fe3f161ccd84a688ba3bae794993f0a6baf
ronnapatp <ronnapat.sri@gmail.com>
2021-10-15 22:42:43 +0700

    Update navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -21,17 +21,17 @@
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <span class="mx-2 text-sm sm:text-md">
-                <a href="/#download">
+                <a href="/about">
                   {About}
                 </a>
               </span>
               <span class="mx-2 text-sm sm:text-md">
-                <a href="/faq">
+                <a href="https://ronnapatpblog.netlify.app/" target="_blank">
                   {Blog}
                 </a>
               </span>
               <span class="mx-2 text-sm sm:text-md">
-                <a href="/">
+                <a href="/contact/">
                   {Contact}
                 </a>
               </span>
