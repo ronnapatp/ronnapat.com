99e2e9832ac70d70af00dd42f234a05a672c4557
ronnapatp <ronnapat.sri@gmail.com>
2021-10-15 22:52:13 +0700

    Add data

---
+++ __test__/data.ts
@@ -0,0 +1,10 @@
+const fullName = "Ronnapat Srivoravilai";
+const nickName = "Pie";
+const socialName = "ronnapatp";
+const framwork = "svelte kit";
+const createBy = "Ronnapat Srivoravilai";
+const twitter = "@ronnapatp";
+const github = "ronnapatp";
+const gmail = "ronnapat.sri@gmail.com";
+
+console.log("Twiiter and Github username if",fullName , "is Twitter : ",twitter,"Github : ",github )
