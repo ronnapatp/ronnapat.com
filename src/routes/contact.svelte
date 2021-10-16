c593a28dce57af8484edda6d5a63663590299803
ronnapatp <ronnapat.sri@gmail.com>
2021-10-16 17:29:36 +0700

    Add contact

---
+++ src/routes/contact.svelte
@@ -0,0 +1,17 @@
+<script>
+    const Twitter = "ronnapatp";
+    const Github = "ronnapatp";
+    const Gmail = "ronnapat.sri@gmail.com";
+
+    import Navbar from "../components/navbar.svelte";
+</script>
+
+
+<Navbar />
+<main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
+<h1 class="text-6xl">Contact me</h1>
+<a href="https://github.com/{Github}">Github : {Github}</a>
+<a href="https://twitter.com/{Twitter}">Twitter : {Twitter}</a>
+<a href="mailto:{Gmail}">Gmail : {Gmail}</a>
+    
+</main>
