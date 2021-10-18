7cfa4d3b2b31d1482f5628e59d19994fabbebefb
ronnapatp <ronnapat.sri@gmail.com>
2021-10-18 20:14:47 +0700

    Update contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -6,6 +6,13 @@
     const fullName = "Ronnapat Srivoravilai";
 
     import Navbar from "../components/navbar.svelte";
+    import Copy from "copy-to-clipboard"
+
+    function copyDiscord() {
+        Copy('ronnapatp#3538')
+        alert('Copy ronnapat discord username to your clipboard!')
+        console.log('Copied!')
+    }
 </script>
 
 <head>
@@ -19,8 +26,19 @@
 <Navbar />
 <main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
 <h1 class="text-6xl">Contact me</h1>
-<a href="https://github.com/{Github}">Github : {Github}</a>
-<a href="https://twitter.com/{Twitter}">Twitter : {Twitter}</a>
-<a href="mailto:{Gmail}">Gmail : {Gmail}</a>
-    
+<p class="w-1/5	">You can contact me bla bla bla blab alb la bla blab alb la bla blab alb la bla blab alb la bla blab alb </p>
+<div class="flex gap-6">
+<a href="mailto:ronnapat.sri@gmail.com" target="_blank">
+    <img src="../../static/mail.png" alt="mail" class="w-20 h-20">
+</a>
+<a href="https://twitter.com/ronnapatp" target="_blank">
+    <img src="../../static/twitter.png" alt="twitter" class="w-20 h-20">
+</a>
+<a href="https://facebook.com/pieronnapatp" target="_blank">
+    <img src="../../static/facebook.png" alt="facebook" class="w-20 h-20">
+</a>
+<button on:click={copyDiscord}>
+    <img src="../../static/discord.png" alt="" class="w-20 h-20">
+</button>
+</div>
 </main>
