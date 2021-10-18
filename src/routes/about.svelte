dc635a0fe66cba08c588af7cea224e87043e75a8
ronnapatp <ronnapat.sri@gmail.com>
2021-10-18 19:51:36 +0700

    Update head

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
     import Nav from '../components/navbar.svelte'
     import About from '../components/about.svelte'
+
+    const Title = "About | Ronnapat Srivoravilai";
+    const fullName = "Ronnapat Srivoravilai"
 </script>
 
+<head>
+    <title>{Title}</title>
+    <meta property="og:url" content="https://ronnapat.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{Title}" />
+    <meta property="og:description" content="Personal website of {fullName}" />
+</head>
 
 <Nav />
 <br><br><br><br>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -2,10 +2,19 @@
     const Twitter = "ronnapatp";
     const Github = "ronnapatp";
     const Gmail = "ronnapat.sri@gmail.com";
+    const Title = "Contact | Ronnapat Srivoravilai";
+    const fullName = "Ronnapat Srivoravilai";
 
     import Navbar from "../components/navbar.svelte";
 </script>
 
+<head>
+    <title>{Title}</title>
+    <meta property="og:url" content="https://ronnapat.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{Title}" />
+    <meta property="og:description" content="Personal website of {fullName}" />
+</head>
 
 <Navbar />
 <main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
