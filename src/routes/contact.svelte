f7af07916c1afee054938795a7acfa04a807eb72
ronnapatp <ronnapat.sri@gmail.com>
2021-10-18 20:16:59 +0700

    Reset page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -13,6 +13,9 @@
         alert('Copy ronnapat discord username to your clipboard!')
         console.log('Copied!')
     }
+    //function copyMail() {
+    //    Copy('ronnapat.sri@gmail.com')
+    //}
 </script>
 
 <head>
@@ -25,20 +28,20 @@
 
 <Navbar />
 <main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
-<h1 class="text-6xl">Contact me</h1>
-<p class="w-1/5	">You can contact me bla bla bla blab alb la bla blab alb la bla blab alb la bla blab alb la bla blab alb </p>
-<div class="flex gap-6">
-<a href="mailto:ronnapat.sri@gmail.com" target="_blank">
-    <img src="../../static/mail.png" alt="mail" class="w-20 h-20">
-</a>
-<a href="https://twitter.com/ronnapatp" target="_blank">
-    <img src="../../static/twitter.png" alt="twitter" class="w-20 h-20">
-</a>
-<a href="https://facebook.com/pieronnapatp" target="_blank">
-    <img src="../../static/facebook.png" alt="facebook" class="w-20 h-20">
-</a>
-<button on:click={copyDiscord}>
-    <img src="../../static/discord.png" alt="" class="w-20 h-20">
-</button>
-</div>
+    <h1 class="text-6xl">Contact me</h1>
+    <p class="w-1/5	">You can contact me bla bla bla blab alb la bla blab alb la bla blab alb la bla blab alb la bla blab alb </p>
+    <div class="flex gap-6">
+        <a href="mailto:ronnapat.sri@gmail.com" target="_blank" >        
+            <img src="../../static/mail.png" alt="mail" class="w-20 h-20">
+        </a>
+        <a href="https://twitter.com/ronnapatp" target="_blank">        
+            <img src="../../static/twitter.png" alt="twitter" class="w-20 h-20">
+        </a>
+        <a href="https://facebook.com/pieronnapatp" target="_blank">        
+            <img src="../../static/facebook.png" alt="facebook" class="w-20 h-20">
+        </a>
+        <button on:click={copyDiscord}>
+            <img src="../../static/discord.png" alt="" class="w-20 h-20">
+        </button>
+    </div>
 </main>
