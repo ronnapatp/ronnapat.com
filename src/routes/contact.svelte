9d8b2caa944f9b24b6698ecec4c95fd9b0835329
ronnapatp <ronnapat.sri@gmail.com>
2021-10-19 13:44:30 +0700

    Update contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -30,21 +30,110 @@
 </head>
 
 <Navbar />
-<main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
-    <h1 class="text-6xl">Contact me</h1>
-    <p class="">You can contact me on link down below! </p>
-    <div class="flex gap-6">
-        <a href="mailto:ronnapat.sri@gmail.com" target="_blank" title="Click to mail me!">        
-            <img src="mail.png" alt="mail" class="w-10 h-10">
-        </a>
-        <a href="https://twitter.com/ronnapatp" target="_blank" title="Click to see my twitter!">        
-            <img src="/twitter.png" alt="twitter" class="w-10 h-10">
-        </a>
-        <a href="https://facebook.com/pieronnapatp" target="_blank" title="Click to see my facebook!">        
-            <img src="/facebook.png" alt="facebook" class="w-10 h-10">
-        </a>
-        <button on:click={copyDiscord} title="Click to copy my discord username!">
-            <img src="/discord.png" alt="" class="w-10 h-10">
-        </button>
-    </div>
+<main class="w-full h-screen flex flex-col gap-2 justify-center items-center ">
+		<!-- Container -->
+		<div class="container mx-auto ">
+			<div class="flex justify-center px-6 my-12">
+				<!-- Row -->
+				<div class="w-full xl:w-3/4 lg:w-11/12 flex border-2 border-yellow-500 border-solid rounded-md  ">
+					<!-- Col -->
+					<div
+						class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
+						style="background-image: url('/myprofile.jpeg')"
+					></div>
+					<!-- Col -->
+					<div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
+						<div class="px-8 mb-4 text-center">
+							<h1 class="pt-4 mb-2 text-2xl">Contact me!</h1>
+							<p class="mb-4 text-sm text-gray-700">
+								You can contact me in email discord or other platform down below, You can also contact me with form down below.
+							</p>
+						</div>
+						<!--<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+							<div class="mb-4">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+									Email
+								</label>
+								<input
+									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="email"
+									type="email"
+									placeholder="Enter Email Address..."
+								/>
+							</div>
+							<div class="mb-6 text-center">
+								<button
+									class="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
+									type="button"
+								>
+									Reset Password
+								</button>
+							</div>
+							<hr class="mb-6 border-t" />
+							<div class="text-center">
+								<a
+									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									href="./register.html"
+								>
+									Create an Account!
+								</a>
+							</div>
+							<div class="text-center">
+								<a
+									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									href="./index.html"
+								>
+									Already have an account? Login!
+								</a>
+							</div>
+						</form>-->
+                        <div class="get-app flex space-x-5 mt-10 justify-center ">
+                    
+                            <a href="https://github.com/ronnapatp" target="_blank" class="apple bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                                <div class="logo">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                                      </svg>
+                                </div>
+                            </a>
+                            <a href="https://twitter.com/ronnapatp" target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                                <div class="logo">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-twitter text-blue-500" viewBox="0 0 16 16">
+                                        <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                                      </svg>
+                                </div>
+                            </a>
+                            <a href="https://facebook.com/pieronnapatp" target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                                  <div class="logo">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-facebook text-blue-600	" viewBox="0 0 16 16">
+                                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                      </svg>
+                                </div>
+                            </a>
+                            <button class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4" on:click={copyDiscord}>
+                                <div class="logo">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-discord" viewBox="0 0 16 16" style="color: #738ADB;">
+                                        <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"/>
+                                      </svg>
+                              </div>
+                            </button>
+                            <!--<a href="https://www.linkedin.com/in/ronnapat-srivoravilai-100238216/" target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                                <div class="logo">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16" style="color: #0077B5;">
+                                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                                      </svg>
+                                </div>
+                          </a>-->
+                            <a href="mailto:ronnapat.sri@gmail.com" target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                                <div class="logo">
+                                <img src="/mail.png" alt="" width="25" height="25">
+                              </div>
+                          </a>
+                        </div>
+                        <br>
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeozq48SAbJkznx9X5NwlLN-XfiVmw5C89y5gHXkCAerddoEg/viewform?embedded=true" width="520" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+					</div>
+				</div>
+			</div>
+		</div>
 </main>
