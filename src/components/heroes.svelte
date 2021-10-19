4bcb153570a70cefcd757d6d732f3276632bfe46
ronnapatp <ronnapat.sri@gmail.com>
2021-10-19 13:05:20 +0700

    Update heroes

--- src/components/heroes.svelte
+++ src/components/heroes.svelte
@@ -2,18 +2,51 @@
     const fullName = "Ronnapat Srivoravilai"
 </script>
 
-<div class="text-center text-black">
-    <p>
-        Hi, My name is
-    </p>
-    <p class="text-6xl">
-        {fullName}
-    </p>
-    <p>
-        here you can find information about "me".
-    </p>
-    <br><br>
-    <center>
-        <img src="/myprofile.jpeg" alt="" width="80%" height="auto">
-    </center>
+<div class="hero bg-white py-16">
+    <!-- container -->
+    <div class="container px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
+        <!-- hero wrapper -->
+        <div class="hero-wrapper grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
+
+            <!-- hero text -->
+            <div class="hero-text col-span-6">
+                <h1 class=" font-bold text-4xl md:text-5xl max-w-xl text-gray-900 leading-tight"><p class="text-xl">My name is</p>{fullName}</h1>
+                <hr class=" w-12 h-1 bg-orange-500 rounded-full mt-8">
+                <p class="text-gray-800 text-base leading-relaxed mt-8 font-semibold"></p>
+                <div class="get-app flex space-x-5 mt-10 justify-center md:justify-start">
+                    
+                    <a href="https://github.com/ronnapatp" target="_blank" class="apple bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                        <div class="logo">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                              </svg>
+                        </div>
+                    </a>
+                    <a href="https://twitter.com/ronnapatp" target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                        <div class="logo">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-twitter text-blue-500" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                              </svg>
+                        </div>
+                    </a>
+                    <a href="https://facebook.com/pieronnapatp" target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
+                        <div class="logo">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-facebook text-blue-600	" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                              </svg>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <!-- hero image -->
+            <div class="hero-image col-span-6">
+                <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                  </svg>
+            </div>
+        </div>
+        <hr>
+    </div>
 </div>
+
