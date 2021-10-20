5e4b0554a7c510984d6259400f8f0de1cc611c41
ronnapatp <ronnapat.sri@gmail.com>
2021-10-20 17:02:43 +0700

    Add projext

---
+++ src/components/project/project.svelte
@@ -0,0 +1,31 @@
+<div class="container">
+    <h1>My project</h1>
+    <br>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+          <h1 class="display-5 fw-bold">My own website</h1>
+          <p class="col-md-8 fs-4">Personal website for me using sveltekit with bootstrap</p>
+          <a href="/project/my-own-website" class="btn btn-warning btn-lg" type="button">Read more</a>
+          <br><br><small class="text-muted">Tag : discordbot bot js javascript</small>
+        </div>
+      </div>
+    
+      <div class="row align-items-md-stretch">
+        <div class="col-md-6">
+          <div class="h-100 p-5 text-white bg-dark rounded-3">
+            <h2>Kaolad discord bot</h2>
+            <p>Disocrd bot for manage my friend discord server name Hi I bored using discord.js with dotenv file</p>
+            <button class="btn btn-outline-warning" type="button">Read more</button>
+            <br><br><small class="text-muted">Tag : discordbot bot js javascript</small>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="h-100 p-5 text-white bg-dark rounded-3">
+            <h2>How to?</h2>
+            <p>How to website for developer using nextjs but in typescript</p>
+            <button class="btn btn-outline-warning" type="button">Read more</button>
+            <br><br><small class="text-muted">Tag : nextjs typescript howto blog</small>
+          </div>
+        </div>
+      </div>
+</div>
---
+++ src/routes/project/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from "../../components/navbar.svelte";
+    import Project from "../../components/project/project.svelte";
+    import Footer from "../../components/footer.svelte";
+</script>
+
+<Navbar />
+<br><br><br><br>
+<Project />
+<Footer />
