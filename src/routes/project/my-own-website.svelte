78b7b4cad694c04f14b954d2005a4d1c62096423
ronnapatp <ronnapat.sri@gmail.com>
2021-10-20 21:39:20 +0700

    Update facebook comment plugin

--- src/routes/project/my-own-website.svelte
+++ src/routes/project/my-own-website.svelte
@@ -84,6 +84,6 @@
         Suggest project idea at <a href="https://github.com/ronnapatp" class="black">github</a>  
     </div>
 
-    <div class="fb-comments" data-href="https://www.ronnapat.com/project/my-own-website" data-width="600" data-numposts="5"></div></div>
+    <div class="fb-comments" data-href="https://www.ronnapat.com/" data-width="600" data-numposts="5"></div></div>
 </body>
 <Footer />
