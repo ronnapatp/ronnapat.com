474a867d1e6fe736d3c1249a355092d1b0983688
ronnapatp <ronnapat.sri@gmail.com>
2021-10-20 11:22:10 +0700

    Add name .js

---
+++ src/lib/name.js
@@ -0,0 +1,11 @@
+export const fullName = "Ronnapat Srivoravilai"
+
+export const nickName = "Pie"
+
+export const socialName = "ronnapatp"
+
+export const facebookUser = "pieronnapatp"
+
+export const githubUser = "ronnapatp"
+
+export const twitterUser = "@ronnapatp"
