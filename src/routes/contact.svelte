b24b9103e14d2f9107c1d750b81abcdc253937e4
ronnapatp <ronnapat.sri@gmail.com>
2021-10-20 15:22:08 +0700

    Update

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -36,7 +36,13 @@
 <div class="container col-xl-10 col-xxl-8 px-4 py-5">
     <div class="row align-items-center g-lg-5 py-5">
       <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 mb-3">Contact me</h1>
+        <h1 class="display-4 fw-bold lh-1 mb-3">
+            Contact me
+            <svg width="35" height="35" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18.1182 14.702L14 15.5C11.2183 14.1038 9.5 12.5 8.5 10L9.26995 5.8699L7.81452 2L4.0636 2C2.93605 2 2.04814 2.93178 2.21654 4.04668C2.63695 6.83 3.87653 11.8765 7.5 15.5C11.3052 19.3052 16.7857 20.9564 19.802 21.6127C20.9668 21.8662 22 20.9575 22 19.7655L22 16.1812L18.1182 14.702Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                
+        </h1>
         <p class="col-lg-10 fs-4">You can contact me on social media or form.</p>
         <span >
             <a href="https://facebook.com/pieronnapatp" target="_blank">
