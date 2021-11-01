77dfacebf3cc89053383725de666a8887cf5a2f7
ronnapatp <ronnapat.sri@gmail.com>
2021-11-01 14:11:57 +0700

    Update fb comment

--- src/routes/project/kaolad-bot.svelte
+++ src/routes/project/kaolad-bot.svelte
@@ -33,13 +33,13 @@
     <meta property="og:description" content="Personal website of {fullName}" />
     <meta property="og:image" content="https://www.ronnapat.com/logo.png" />
     <meta property="og:keywords" content="{fullName} , Ronnapat , ronnapatp , srivoravilai ">
-    <meta property="fb:app_id" content="940860116773848"/>
+    <meta property="fb:app_id" content="924964718457366"/>
 </head>
 
 <Navbar />
 <body>
     <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="uYMh4Izw"></script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=924964718457366&autoLogAppEvents=1" nonce="SUtkN3EF"></script>
 <br><br>
 <div class="container">
 <a href="/project/" class="black">Project</a> / <a href="#" class="black">{Name}</a>
