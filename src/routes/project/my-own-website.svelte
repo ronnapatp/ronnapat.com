7e8e6aaefbbe37f8e4cc6c66db202097cede8ecd
ronnapatp <ronnapat.sri@gmail.com>
2021-11-01 13:37:59 +0700

    Update fb comment

--- src/routes/project/my-own-website.svelte
+++ src/routes/project/my-own-website.svelte
@@ -40,7 +40,7 @@
 <Navbar />
 <body>
     <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="uYMh4Izw"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=924964718457366&autoLogAppEvents=1" nonce="SUtkN3EF"></script>
 <br><br>
 <div class="container">
 <a href="/project/" class="black">Project</a> / <a href="#" class="black">{Name}</a>
@@ -85,6 +85,6 @@
         Suggest project idea at <a href="https://github.com/ronnapatp" class="black">github</a>  
     </div>
 
-    <div class="fb-comments" data-href="https://www.ronnapat.com/" data-width="600" data-numposts="5"></div></div>
+<div class="fb-comments" data-href="https://www.ronnapat.com/project/my-own-website" data-width="600" data-numposts="5"></div>
 </body>
 <Footer />
