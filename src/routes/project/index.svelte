3a350a3beade6a7e1a7be6cf7871919c4dee28ca
ronnapatp <ronnapat.sri@gmail.com>
2021-11-02 12:05:13 +0700

    Update meta

--- src/routes/project/index.svelte
+++ src/routes/project/index.svelte
@@ -6,6 +6,12 @@
 
 <head>
     <title>Project | Ronnapat Srivoravilai</title>
+    <meta property="og:url" content="https://ronnapat.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Project | Ronnapat Srivoravilai" />
+    <meta property="og:description" content="Personal website of Ronnapat Srivoravilai" />
+    <meta property="og:image" content="https://www.ronnapat.com/logo.png" />
+    <meta property="og:keywords" content="Ronnapat Srivoravilai , Ronnapat , ronnapatp , srivoravilai ">
 </head>
 
 <Navbar />
