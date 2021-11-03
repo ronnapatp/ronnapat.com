cec45fd40bf7d7f82ea6b5bb4bdb51aebf255c69
ronnapatp <ronnapat.sri@gmail.com>
2021-11-03 17:01:55 +0700

    Update about me

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -95,10 +95,36 @@ a:hover{
         </span>
       </div>
     </div>
+    <p> </p>
+    <h3>EDUCATION</h3>
+    <p>
+    <li> Chulalongkorn University Demonstration Elementary School </li>
+    <li> Chulalongkorn University Demonstration Secondary School</li>
+    </p>
+    <h3>TECHNOLOGIES AND LANGUAGES</h3>
+    <p>
+      <li><b>Languages</b> : JavaScript, Node.js, TypeScript, Python, HTML(Storng), CSS </li>
+      <li><b>Frameworks</b> : Astro, ReactJS, NextJS, NestJS, Svelte, Svelte kit, TailwindCSS, Bootstrap, Vue </li>
+      <li><b>Tools</b> : Git, Framework, Windows 11, Powershell, Command prompt , Ubuntu , macOS , Kitty terminal</li>
+      <li><b>Other</b> : Firebase, Vercel, Netlify, Godaddy, Github Actions, Github copilot, Github codespace , Google domain </li>
+    </p>
+    <h3>INTERESTS</h3>
+    <p>
+      <li><b>Games</b> : FPS, Offline game, Moba, Puzzle, Game on play station</li>
+      <li><b>Music</b> : EDM, Rock, Musicial</li>
+      <li><b>Pets</b> : I'm looking for have korgi dog</li>
+      <li><b>Social media</b> : Facebook, Instagram, Twitter</li>
+      <li><b>Typing</b> : TH 42wpm ENG 55 wpm</li>
+      <li><b>Keyboard layout</b> : 🇹🇭 <a href="https://manoonchai.com/" target="_blank" class="text-decoration-none">Manoonchai</a> 🇺🇸QWERTY</li>
+    </p>
+    <h3>CONTACT ME</h3>
+    <p><a href="/contact">Contact page</a></p>
+    <p> </p>
+    <br>
   </div>
   <br><br><br>
-  <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-    <!--<a href="/ronnapat.pdf" download>Download as PDF</a>-->
+  <!-- <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+    
   
     <h1>Ronnapat Srivoravilai</h1>
     <p>
@@ -132,6 +158,6 @@ a:hover{
     <li><b>Github</b> : <a href="https://github.com/ronnpatp" target="_blank">ronnapat</a></li>
     <p> </p>
     <br><br>
-  </div>
+  </div> -->
   
 </div>
