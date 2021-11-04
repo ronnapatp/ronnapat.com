0aaea0e4852794b508ebfc91923c154200237eef
ronnapatp <ronnapat.sri@gmail.com>
2021-11-04 15:58:07 +0700

    Add form with supabase

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,5 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(
    import.meta.env.VITE_SUPABASE_URL,
    import.meta.env.VITE_SUPABASE_ANON_KEY
+
+//    const supabaseUrl = process.env.VITE_SUPABASE_URL
+// const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
 )
 export default supabase
--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     vite: {
       define: {
-          'process.env': process.env,
+          'meta.env': meta.env,
       },
     },
     target: '#svelte',
