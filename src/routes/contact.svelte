9bd0649ee29be4b71e1f3cbafd78a743335726fc
ronnapatp <ronnapat.sri@gmail.com>
2021-11-04 19:08:31 +0700

    Update form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -33,8 +33,8 @@
 	}
   async function sendData() {
     if (Robot == 8) {
-      alert('Thank you for your message! I will answer you as soon as possible.')
-      window.location.replace('/contact');
+     alert('Thank you for your message! I will answer you as soon as possible.')
+     window.location.replace('/contact');
       const { data, error } = await supabase
       .from('games')
       .insert([
@@ -131,12 +131,12 @@
                 </svg>
             </a>
             &nbsp;
-            <a href="https://bit.ly/3ncfZvC" target="_blank">
+            <!-- <a href="https://bit.ly/3ncfZvC" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-newspaper black" viewBox="0 0 16 16">
                     <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z"/>
                     <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z"/>
                   </svg>
-            </a>
+            </a> -->
           </span>
       </div>
       <div class="col-md-10 mx-auto col-lg-5">
