a2b010ca1edfbb6e1c27a0f2ff8b1408231d751c
ronnapatp <ronnapat.sri@gmail.com>
2021-11-05 11:57:25 +0700

    Update

--- src/lib/db.js
+++ src/lib/db.js
@@ -14,4 +14,4 @@ import { createClient } from '@supabase/supabase-js'
 
  const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
  const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-export const supabase = createClient( SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient( SUPABASE_URL, SUPABASE_ANON_KEY )
