438189b2038455e4032d07109b454d66aac3876d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-09 14:28:46 +0700

    Add thai school

---
+++ src/routes/school/subject/thai/1.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Nav from '../../../../components/navbar.svelte';
+    import Footer from '../../../../components/footer.svelte';
+</script>
+
+<svelte:head>
+    <title>สไลด์กาพย์เรื่องพระไชยสุริยา ครั้งที่ ๒</title>
+    <meta property="og:url" content="https://ronnapat.com/school/subject/thai/1" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="สไลด์กาพย์เรื่องพระไชยสุริยา ครั้งที่ ๒" />
+</svelte:head>
+
+<Nav />
+<br><br>
+<div class="container">
+    <img src="/school/thai/1.png" alt="" class="img-thumbnail">
+    <img src="/school/thai/2.png" alt="" class="img-thumbnail">
+    <img src="/school/thai/3.png" alt="" class="img-thumbnail">
+    *ไม่จำคำพระเจ้า = ไม่ทำตามหลักการของศาสนา
+    <img src="/school/thai/4.png" alt="" class="img-thumbnail">
+</div>
+
+
+
+<Footer />
