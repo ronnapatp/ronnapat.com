efd77be8638f589b7a199de7e12e3984b97bbfdb
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-22 11:24:52 +0700

    Make trap for rickroll

--- src/routes/contacts.svelte
+++ src/routes/contacts.svelte
@@ -1,1 +1,8 @@
-<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<head>
+    <meta property="og:title" content="Contacts | Ronnapat Srivoravilai" />
+    <meta property="og:description" content="Personal website of Ronnapat Srivoravilai" />
+</head>
+<video width="90%" height="90%" controls autoplay>
+    <source src="/rickroll.mp4" type="video/mp4">
+  </video>
+<button on:click={() => location.href = "https://youtu.be/VbUuB1aN2DA?t=17"} class="text-center" >Stop playing rickroll</button>
