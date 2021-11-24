9e4c261e599cb2d3bd90088929953bc6014a0084
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-24 21:07:13 +0700

    Try login

---
+++ src/routes/admin/login.svelte
@@ -0,0 +1,29 @@
+<script>
+    let usename = "ronnapatp"
+    let password = "pie"
+
+    let user;
+    let pass;
+
+    let admin = false
+    let login = true
+    function submit(){
+        if ( user === usename && pass === password ) {
+            login = false
+            admin = true
+        } else {
+            alert("Wrong")
+        }
+    }
+</script>
+
+{#if login}
+    <form>
+        <input type="text" bind:value={user}>
+        <input type="password" bind:value={pass}>
+        <input type="submit" on:click={submit}>
+    </form>
+{/if}
+{#if admin}
+    <h1>Hello</h1>
+{/if}
