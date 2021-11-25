600faf69d14e25d125d039f738a32e271dec8c66
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-25 13:07:20 +0700

    try profile

---
+++ src/routes/profile/index.svelte
@@ -0,0 +1,43 @@
+<script>
+</script>
+<style>
+    .navbar-apple{
+        background-color: #636366;
+        color: #D1D1D6;
+        font-size: 1.25rem;
+    }
+    a{
+        color: #D1D1D6;
+        text-decoration: none;
+    }
+    .drop{
+        background-color: #636366; 
+        border-style: solid;
+        border-width: 0.15rem; 
+        border-color: #D1D1D6; 
+        color: white;
+        padding: 0.2rem 0.40rem;   
+    }
+</style>
+<body>
+    <nav class="navbar navbar-light navbar-apple">
+        <div class="container">
+          <a>RONNAPAT SRIVORAVILAI</a>
+          <form class="inline-block">
+               <small>
+                เลือกภาษาอื่น หากต้องการดูเนื้อหาในภาษาของคุณ
+                <b class="btn-group">
+                    <button class="drop dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Small button
+                    </button>
+                    <ul class="dropdown-menu" style="background-color: #636366;">
+                        <li><a class="dropdown-item" href="#">ไทย</a></li>
+                        <li><a class="dropdown-item" href="#">อังกฤษ</a></li>
+                    </ul>
+                  </b>
+               </small>
+               
+          </form>
+        </div>
+      </nav>
+</body>
