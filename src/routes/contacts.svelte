273aefd97d7ee758b980342759247c674d671ca5
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-28 12:48:12 +0700

    Update rick roll

--- src/routes/contacts.svelte
+++ src/routes/contacts.svelte
@@ -1,8 +1,9 @@
 <head>
-    <meta property="og:title" content="Contacts | Ronnapat Srivoravilai" />
-    <meta property="og:description" content="Personal website of Ronnapat Srivoravilai" />
+    <meta property="og:title" content="Contact me" />
+    <meta property="og:description" content="Contact Ronnapat Srivoravilai" />
 </head>
+<!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  allowfullscreen></iframe> -->
 <video width="90%" height="90%" controls autoplay>
     <source src="/rickroll.mp4" type="video/mp4">
   </video>
-<button on:click={() => location.href = "https://youtu.be/VbUuB1aN2DA?t=17"} class="text-center" >Stop playing rickroll</button>
+<button on:click={() => location.href = "https://youtu.be/VbUuB1aN2DA?t=17"} class="text-center" >Stop playing</button>
