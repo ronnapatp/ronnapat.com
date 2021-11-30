b3ea4046066ed5842d8bacfd2471aa0c2cc4477a
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-30 11:44:52 +0700

    Delete comment in contact page

--- src/routes/contact/send.svelte
+++ src/routes/contact/send.svelte
@@ -13,15 +13,6 @@
         alert('Copy ronnapat discord username to your clipboard!')
         console.log('Copied!')
     }
-    //function copyMail() {
-    //    Copy('ronnapat.sri@gmail.com')
-    //}
-
-
-  
-  // function myForm {
-  //   reset()
-  // }
 </script>
 
 <svelte:head>
@@ -94,13 +85,6 @@
                 <path d="M2 17V7C2 5.89543 2.89543 5 4 5H20C21.1046 5 22 5.89543 22 7V17C22 18.1046 21.1046 19 20 19H4C2.89543 19 2 18.1046 2 17Z" stroke="currentColor" stroke-width="1.5"/>
                 </svg>
             </a>
-            &nbsp;
-            <!-- <a href="https://bit.ly/3ncfZvC" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-newspaper black" viewBox="0 0 16 16">
-                    <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z"/>
-                    <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z"/>
-                  </svg>
-            </a> -->
           </span>
       </div>
       <div class="col-md-10 mx-auto col-lg-5">
