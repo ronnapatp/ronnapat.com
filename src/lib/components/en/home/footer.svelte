7fb9ecbfdd08222951a22cdb12935604fc62e7e0
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 17:07:47 +0700

    Add hr

--- src/lib/components/en/home/footer.svelte
+++ src/lib/components/en/home/footer.svelte
@@ -17,6 +17,7 @@ import { fullName } from '$lib/name';
         showAlert = false
     }
 </script>
+<hr>
 <footer class="relative bg-blueGray-200 pt-8 pb-6">
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap text-left lg:text-left">
