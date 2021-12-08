ebaba6b091cbc11153f3b43dfbfa223bc81a74da
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 21:29:22 +0700

    Add 404 page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,17 +1,17 @@
-<svelte:head>
-    <title>404 - Page not found</title>
-</svelte:head>
-
-<br><br><br>
-
-<div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/logo-no-bg.png" alt="" width="72" height="72">
-    <h1 class="display-5 fw-bold">404 - Page not found</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">We cannot find this page for you but don't worry you can go back to home pag eby click button down below.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <a href="/" type="button" class="btn btn-warning btn-lg px-4 gap-3">Back to home page!</a>
-      </div>
+<div class="h-screen w-screen bg-white flex items-center">
+	<div class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+   		<div class="max-w-md">
+      		<div class="text-5xl font-dark font-bold">404</div>
+            <p
+              class="text-2xl md:text-3xl font-light leading-normal"
+            >Sorry we couldn't find this page. </p>
+          <p class="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+          
+          <a href="/" class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">back to homepage</a>
     </div>
+      <div class="max-w-lg">
+        <i class="bi bi-emoji-frown text-9xl"></i>
+      </div>
+    
   </div>
-
+</div>
