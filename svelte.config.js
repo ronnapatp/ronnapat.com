3c7bd661ae205b444957ce63b511b966073eb609
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 21:14:44 +0700

    Delete mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
 import preprocess from 'svelte-preprocess'
 import vercel from '@sveltejs/adapter-vercel'
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  "extensions": [".svelte", ...mdsvexConfig.extensions],
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: [
@@ -14,7 +11,6 @@ const config = {
     }),
   ],
 
-  preprocess: [mdsvex(mdsvexConfig)],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
