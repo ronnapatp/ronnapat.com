fa6bdc63bf131d8791f77049daa31ebaee6cdae6
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 21:32:36 +0700

    Delete comment

--- src/routes/th/index.svelte
+++ src/routes/th/index.svelte
@@ -1,16 +1,9 @@
 <script>
     import Navbar from '$lib/components/th/home/navbar.svelte'
 </script>
-
-
-<!-- <Navbar /> -->
-<!-- component -->
 <div class="contenair bg-cover min-h-screen w-full flex justify-center items-center"
 style="background-image: url('/heroes.png');'">
-  
-<!-- card -->
   <div class="w-1/2 bg-black p-5 rounded-xl bg-opacity-60 backdrop-filter backdrop-blur-lg">
     <h1 class="text-9xl text-center text-white">Coming soon</h1>
-
     </div>
   </div>
