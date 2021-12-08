456233d39341b7228a357dec80b716178953c37e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 21:20:30 +0700

    Change to use {}

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    const fullName = "Ronnapat Srivoravilai";
-    const title = "HOME | Ronnapat Srivoravilai"
+    import { fullName } from '$lib/name'
     
     import Navbar from '$lib/components/en/home/navbar.svelte';
     import Heroes from '$lib/components/en/home/heroes.svelte';
@@ -9,16 +8,16 @@
 
 
 <svelte:head>
-    <title>{title}</title>
+    <title>Home | {fullName}</title>
     <meta property="og:url" content="https://ronnapat.com/" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="{title}" />
+    <meta property="og:title" content="Home | {fullName}" />
     <meta property="og:description" content="Personal website of {fullName}" />
     <meta property="og:image" content="https://ronnapat.com/logo.png" />
     <meta property="og:keywords" content="{fullName} , Ronnapat , ronnapatp , srivoravilai ">
     <meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:site" content="@ronnapatp">
-	<meta name="twitter:title" content="{title}">
+	<meta name="twitter:title" content="Home | {fullName}">
 	<meta name="twitter:description" content="Personal website of {fullName}">
 	<meta name="twitter:creator" content="@ronnapatp">
     <meta name="twitter:image:src" content="https://ronnapat.com/logo.png">
