186eb6e79483cad63c66a2e2824cc7b2bad65bfa
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 21:29:43 +0700

    Delete space

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -8,10 +8,9 @@
           <p class="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
           
           <a href="/" class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">back to homepage</a>
-    </div>
+      </div>
       <div class="max-w-lg">
         <i class="bi bi-emoji-frown text-9xl"></i>
       </div>
-    
   </div>
 </div>
