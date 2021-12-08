40fdf79be8ada4fed8d03bdba0b590593520c961
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-08 17:51:01 +0700

    Add head tag in contact page

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -1,8 +1,29 @@
 <script>
+    const title = "Contact | Ronnapat Srivoraivlai";
+
+    import { fullName } from "$lib/name";
     import Navbar from "$lib/components/en/contact/navbar.svelte";
     import Main from "$lib/components/en/contact/main.svelte";
     import Footer from "$lib/components/en/footer.svelte";
 </script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta property="og:url" content="https://ronnapat.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{title}" />
+    <meta property="og:description" content="Personal website of {fullName}" />
+    <meta property="og:image" content="https://ronnapat.com/logo.png" />
+    <meta property="og:keywords" content="{fullName} , Ronnapat , ronnapatp , srivoravilai ">
+    <meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@ronnapatp">
+	<meta name="twitter:title" content="{title}">
+	<meta name="twitter:description" content="Personal website of {fullName}">
+	<meta name="twitter:creator" content="@ronnapatp">
+    <meta name="twitter:image:src" content="https://ronnapat.com/logo.png">
+	<meta name="twitter:domain" content="ronnapat.com">
+</svelte:head>
+
 
 <Navbar />
 <Main />
