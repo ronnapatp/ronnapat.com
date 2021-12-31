dea56a5430864774ca55d0762295f1b103513800
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-31 20:28:27 +0700

    Update navbar

--- components/navbar.js
+++ components/navbar.js
@@ -79,7 +79,7 @@ export default function Example() {
             </a>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
               <span className="sr-only">Open menu</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
@@ -164,7 +164,7 @@ export default function Example() {
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
-                <a href='/' className='inline-flex space-x-2'>
+                <div className='inline-flex space-x-2'>
                   <img
                     className="h-8 w-auto"
                     src="https://ronnapat.com/favicon.ico"
@@ -173,9 +173,9 @@ export default function Example() {
                   <span className='text-2xl'>
                     Ronnapat Srivoravilai
                   </span>
-                </a>
+                </div>
                 <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
                     <span className="sr-only">Close menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
