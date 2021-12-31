d2301b8738ed50a78a655b3ed7796d71a0669384
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-31 21:19:07 +0700

    Update header

---
+++ components/header.js
@@ -0,0 +1,24 @@
+export default function Header() {
+    return(
+        <div className="w-full bg-center bg-no-repeat bg-cover bg-[url('https://ronnapat.com/heroes.png')]">
+            <div className='bg-opacity-50 bg-black'>
+                <div className="container flex flex-col px-6 lg:px-40 py-4 mx-auto md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+                    <div className="right-0 hidden md:block md:w-[40%] md:mr-20">
+                        <img className="w-full h-full max-w-2xl rounded-md" src="/cover.jpeg" alt="My image" />
+                    </div>
+                    <div className="flex flex-col items-center w-full md:flex-row">
+                        <div className="mt-40 mb-40 md:mb-0 md:mt-0 md:order-2">
+                            <h1 className="text-6xl text-center md:text-left font-medium tracking-wide text-gray-800 dark:text-white md:text-7xl">Ronnapat Srivoravilai</h1>
+                            <p className="mt-4 text-gray-100 dark:text-gray-200">I create website bot and much more! You can view my project in my blog</p>
+                            <div className="mt-6">
+                                <a href="#" className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">
+                                    Learn more
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,99 +1,11 @@
+import Header from '../components/header'
 import Head from '../components/meta'
 
 export default function Home() {
-  return (<>
-    
-    <div className="flex flex-col items-center justify-center min-h-screen">
+  return (<>    
       <Head title='Home' image='/favicon.ico' />
-      <div className="w-full bg-center bg-no-repeat bg-cover bg-[url('https://ronnapat.com/heroes.png')]">
-        <div className='bg-opacity-50 bg-black'>
-        <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
-        <div className="flex flex-col items-center w-full md:flex-row md:w-1/2">
-            <div className="max-w-lg md:mx-12 md:order-2">
-                <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">Ronnapat Srivoravilai</h1>
-                <p className="mt-4 text-gray-600 dark:text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae laudantium quod rem voluptatem eos accusantium cumque.</p>
-                <div className="mt-6">
-                    <a href="#" className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Download from App Store</a>
-                </div>
-            </div>
-        </div>
 
-        <div className="flex items-center justify-center w-full h-96 md:w-1/2">
-            <img className="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="apple watch photo" />
-        </div>
-    </div>
-        </div>
-      </div>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+      <Header />
     </>
   )
 }
