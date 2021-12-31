6881a866b17e60829d75eb3863be5a0898f7722e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-31 20:26:04 +0700

    Add name to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -64,13 +64,18 @@ export default function Example() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center py-4 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
+            <a href="#" className='inline-flex space-x-2'>
               <img
                 className="h-8 w-auto sm:h-10"
                 src="https://ronnapat.com/favicon.ico"
                 alt=""
               />
+              <span className='text-2xl mt-1 hidden lg:block'>
+                Ronnapat Srivoravilai
+              </span>
+              <span className='text-2xl sm:mt-1 block lg:hidden'>
+                Ronnapat
+              </span>
             </a>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
@@ -159,13 +164,16 @@ export default function Example() {
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
-                <div>
+                <a href='/' className='inline-flex space-x-2'>
                   <img
                     className="h-8 w-auto"
                     src="https://ronnapat.com/favicon.ico"
                     alt="Workflow"
                   />
-                </div>
+                  <span className='text-2xl'>
+                    Ronnapat Srivoravilai
+                  </span>
+                </a>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
