b4e6eb8dc91784e6ec7b347fa3670b7dff7d86de
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-31 20:57:26 +0700

    Update header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from '../components/meta'
-import Navbar from '../components/navbar'
 
 export default function Home() {
   return (<>
-    <Navbar />
+    
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Head title='Home' image='/favicon.ico' />
       <div className="w-full bg-center bg-no-repeat bg-cover bg-[url('https://ronnapat.com/heroes.png')]">
-        <div className='mt-36'>
-          <h1 className='text-4xl text-center'>Ronnapat Srivoravilai</h1>
+        <div className='bg-opacity-50 bg-black'>
+        <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+        <div className="flex flex-col items-center w-full md:flex-row md:w-1/2">
+            <div className="max-w-lg md:mx-12 md:order-2">
+                <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">Ronnapat Srivoravilai</h1>
+                <p className="mt-4 text-gray-600 dark:text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae laudantium quod rem voluptatem eos accusantium cumque.</p>
+                <div className="mt-6">
+                    <a href="#" className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Download from App Store</a>
+                </div>
+            </div>
+        </div>
+
+        <div className="flex items-center justify-center w-full h-96 md:w-1/2">
+            <img className="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="apple watch photo" />
+        </div>
+    </div>
         </div>
       </div>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
