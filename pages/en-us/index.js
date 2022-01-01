d0ca7893d8e27150684756ec8dfed2facdec7770
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-01 12:29:39 +0700

    Add head to homepage

--- pages/en-us/index.js
+++ pages/en-us/index.js
@@ -1,5 +1,6 @@
 import Header from "../../components/header";
 import Navbar from '../../components/navbar'
+import Head from '../../components/meta'
 import {
     langenus
 } from '../../script/main'
@@ -7,6 +8,7 @@ import {
 export default function Home() {
     return(
         <>
+            <Head title='Home' image='/favicon.ico' />
             <Navbar lantoshow={langenus} pathname='en-us' />
             <Header name='Ronnapat Srivoravilai' description='I create website bot and much more! You can view my project in my blog' />
         </>
