c78dbe87fd689d43fb665ded1080694f0c1ae3fc
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-01 13:05:51 +0700

    Delete dark:

--- components/header.js
+++ components/header.js
@@ -13,8 +13,8 @@ export default function Header({
                     </div>
                     <div className="flex flex-col items-center w-full md:flex-row">
                         <div className="mt-40 mb-40 md:mb-0 md:mt-0 md:order-2">
-                            <h1 className="text-6xl text-center md:text-left font-medium tracking-wide text-gray-800 dark:text-white md:text-7xl">{name}</h1>
-                            <p className="mt-4 text-gray-100 dark:text-gray-200">{description}</p>
+                            <h1 className="text-6xl text-center md:text-left font-medium tracking-wide text-white md:text-7xl">{name}</h1>
+                            <p className="mt-4 text-gray-100">{description}</p>
                             <div className="mt-6">
                                 <a href={bthref} className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-sky-500 rounded-md md:inline hover:bg-sky-400">
                                     {button}
