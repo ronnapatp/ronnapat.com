b9a40e3e2a9cd57ff9f9e8215d0a47c05d37b5cc
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-01 14:27:45 +0700

    Update skill

--- components/skill.js
+++ components/skill.js
@@ -16,26 +16,84 @@ export default function Example() {
         id: 2,
         image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
       },
+      {
+          id: 3,
+          image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
+      },
+      {
+          id: 4,
+          image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg'
+      },
+      {
+          id: 5,
+          image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg'
+      },
+      {
+          id: 6,
+          image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg'
+      }
     ],
-    Popular: [
+    'Framework': [
+        {
+            id: 1,
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg'
+        },
+        {
+            id: 2,
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'
+        },
+        {
+            id: 3,
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg'
+        },
+        {
+            id: 4,
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg'
+        }
+    ],
+    'Programming tools': [
       {
         id: 1,
-        title: 'Is tech making coffee better or worse?',
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
       },
       {
         id: 2,
-        title: 'The most innovative things happening in coffee',
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg',
       },
+      {
+        id: 3,
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg' 
+      },
+      {
+        id: 4,
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
+      },
+      {
+        id: 5,
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/debian/debian-original.svg'
+      },
+      {
+        id: 6,
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'
+      },
+      {
+        id: 7,
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'
+      }
     ],
-    Trending: [
+    Other: [
       {
         id: 1,
-        title: 'Ask Me Anything: 10 answers to your questions about coffee',
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg',
       },
       {
         id: 2,
-        title: "The worst advice we've ever heard about coffee",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-plain.svg",
       },
+      {
+        id: 3,
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apple/apple-original.svg'
+      }
     ],
   })
 
@@ -79,8 +137,6 @@ export default function Example() {
                     className="relative p-3 rounded-md hover:bg-coolGray-100 inline-flex"
                   >
                     <img src={item.image} className='w-24 h-24' />
-
-
                   </li>
                 ))}
               </ul>
