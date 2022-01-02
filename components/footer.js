3c992a64a94056f3803df30b056d3ca443ee7a89
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-02 17:19:58 +0700

    Update input

--- components/footer.js
+++ components/footer.js
@@ -27,7 +27,7 @@ export default function footer(){
             <div className="text-center pt-10 sm:pt-12 font-light flex items-center justify-center">
                 <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                     <div className="relative">
-                        <input type="text" id="&quot;form-subscribe-Subscribe" className="rounded-lg border-transparent flex-1 appearance-none border border-sky-300 w-full py-2 px-4 bg-white text-gray-700 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent" placeholder="Email"/>
+                        <input type="text" className="rounded-lg border-transparent flex-1 appearance-none border border-sky-300 w-full py-2 px-4 bg-white text-gray-700 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent" placeholder="Email"/>
                         </div>
                         <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-sky-600 rounded-lg shadow-md hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-200" type="submit">
                             Subscribe
