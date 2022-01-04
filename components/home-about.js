81d55ad231bdeb4edbad5f660a1d17b50fe2c2e2
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-04 18:12:51 +0700

    Update dark mode

--- components/home-about.js
+++ components/home-about.js
@@ -4,11 +4,11 @@ export default function Homeabout({
 }) {
     return(
         <>
-            <div className="text-center py-16 sm:py-20 md:py-28 sm:px-4 lg:px-72 home-bg-gd">
-                <h1 className="text-4xl md:text-5xl font-semibold">
+            <div className="text-center dark:bg-slate-700 py-16 sm:py-20 md:py-28 sm:px-4 lg:px-72 bg-white">
+                <h1 className="text-4xl md:text-5xl font-semibold dark:text-white">
                     {title}
                 </h1>
-                <div className="text-xl md:text-3xl font-normal">
+                <div className="text-xl md:text-3xl font-normal dark:text-slate-300">
                     {children}
                 </div>
             </div>
