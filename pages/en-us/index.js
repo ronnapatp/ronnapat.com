f963bb111878b49e43822bd0e998368dd5d48f18
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-04 08:48:48 +0700

    Fix text

--- pages/en-us/index.js
+++ pages/en-us/index.js
@@ -15,7 +15,7 @@ export default function Home() {
             <Navbar lantoshow={langenus} pathname='en-us' />
             <Header name='Ronnapat Srivoravilai' description='I create website bot and much more! You can view my project in my blog' button='Learn more' bthref='#' />
             <Homeabout title='Who am I ?'>
-                I am a student in bangkok,thailand. I like to create website bot and application. I am currently create bottlecrush app (View it on <a href="https://github.com/bottlecrush" className="hover:underline hover:text-sky-500">github</a>)
+                I am a student in bangkok,thailand. I like to create website bot and application. I am currently working on bottlecrush app (View it on <a href="https://github.com/bottlecrush" className="hover:underline hover:text-sky-500">github</a>)
             </Homeabout>
             <center>
                 <Skill />
