cfedb2e34e9f904ace48e24cdc62b1c1bed9370d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-05 15:18:20 +0700

    Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [
