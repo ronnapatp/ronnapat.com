46be3cb7e9af8c268246081de376c84440cba402
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-05 16:05:03 +0700

    Update project css

--- components/project.js
+++ components/project.js
@@ -10,16 +10,16 @@ export default function () {
                 </div>
                 <div className="md:inline-flex md:space-x-5">
                     <div className="p-6 max-w-screen md:max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-2 md:mb-0">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bottle crush system</h5>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Bottle crush system help world clean plastic. We have application and website click down below to learn more.</p>
                         <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-sky-500 dark:bg-opacity-20 rounded-full hover:bg-sky-600 dark:bg-sky-600 dark:hover:bg-sky-500/20">
                             Read more
                             <ArrowRightIcon className="ml-2 -mr-1 w-4 h-4" />
                         </a>
                     </div>
                     <div className="p-6 max-w-screen md:max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-2 md:mb-0">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">My website ronnapat.com</h5>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Since last year I create 2 version of my website and in this year I create version 1.2.0</p>
                         <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-sky-500 dark:bg-opacity-20 rounded-full hover:bg-sky-600 dark:bg-sky-600 dark:hover:bg-sky-500/20">
                             Read more
                             <ArrowRightIcon className="ml-2 -mr-1 w-4 h-4" />
