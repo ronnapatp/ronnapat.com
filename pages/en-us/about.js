934b819de92ec3fa3cdac7206dd46e9a23ad5526
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-06 16:18:29 +0700

    Update meta

--- pages/en-us/about.js
+++ pages/en-us/about.js
@@ -1,3 +1,4 @@
+import Head from '../../components/meta'
 import Navbar from '../../components/navbar'
 import About from "../../components/about";
 import Footer from '../../components/footer';
@@ -8,6 +9,7 @@ import {
 export default function () {
     return(
         <div className='dark:bg-slate-800'>
+            <Head title='About' image='/meta.png' />
             <Navbar lantoshow={langenus} pathname='en-us' page='/about' />
             <About />
             <Footer paht='en-us' />
