d35a03c11eb5d95977dfd43bfe12749f6d0ded36
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-06 15:55:46 +0700

    update goto blog

--- components/go-blog.js
+++ components/go-blog.js
@@ -1,4 +1,4 @@
-export default function Example() {
+export default function() {
     return (
       <div className="bg-gray-50 dark:bg-slate-700">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
