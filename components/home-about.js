9c3db4101e97ee25eb102413a3a60c7feff69047
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-06 16:20:53 +0700

    udpate button

--- components/home-about.js
+++ components/home-about.js
@@ -4,7 +4,7 @@ export default function Homeabout({
 }) {
     return(
         <>
-            <div className="text-center dark:bg-slate-800 py-16 sm:py-20 md:py-28 sm:px-4 lg:px-72 bg-white">
+            <div className="text-center dark:bg-slate-800 py-16 sm:py-20 md:py-28 sm:px-4 lg:px-72 bg-white" id="about">
                 <h1 className="text-4xl md:text-5xl font-semibold dark:text-white">
                     {title}
                 </h1>
--- pages/en-us/index.js
+++ pages/en-us/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <>
             <Head title='Home' image='/meta.png' />
             <Navbar lantoshow={langenus} pathname='en-us' page='/' />
-            <Header name='Ronnapat Srivoravilai' description='I create website bot and much more! You can view my project in my blog' button='Learn more' bthref='#' />
+            <Header name='Ronnapat Srivoravilai' description='I create website bot and much more! You can view my project in my blog' button='Learn more' bthref='#about' />
             <Homeabout title='Who am I ?'>
                 I am a student in bangkok ,Thailand. I like to create website bot and application. I am currently working on bottlecrush app (View it on <a href="https://github.com/bottlecrush" className="hover:underline hover:text-sky-500 dark:hover:text-sky-400">github</a>)
             </Homeabout>
