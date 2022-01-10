426e16961f6ff570ae80bac0f79a231f76aaba73
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-10 17:02:35 +0700

    Update cookies policy

--- components/cookie.js
+++ components/cookie.js
@@ -1,7 +1,7 @@
 export default function () {
     return(
         <div className="relative container mx-auto bg-white px-4">
-            <div className="max-w-prose mx-auto py-8 prose-h1:md:text-6xl prose-h1:text-4xl prose-h1:font-semibold prose-p:text-gray-600 prose-h2:text-2xl prose-h2:md:text-4xl">
+            <div className="max-w-prose mx-auto py-8 prose-h1:md:text-6xl prose-h1:text-4xl prose-h1:font-semibold prose-p:text-gray-600 prose-h2:text-2xl prose-h2:md:text-4xl prose-a:text-gray-600 hover:prose-a:text-sky-500 hover:prose-a:underline">
                 <h1>
                     Cookies Policy
                 </h1>
@@ -17,7 +17,16 @@ export default function () {
                         </a>
                     </h2>
                     <p className="mt-10">
-                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                        Cookies are text files with small pieces of data — like a username and password — that are used to identify your computer as you use a computer network. Specific cookies known as HTTP cookies are used to identify specific users and improve your web browsing experience.
+                        <br /><br />
+                        Data stored in a cookie is created by the server upon your connection. This data is labeled with an ID unique to you and your computer.
+                        <br /><br />
+                        When the cookie is exchanged between your computer and the network server, the server reads the ID and knows what information to specifically serve to you.
+                        <br /><br />
+                        From :
+                        <a className="ml-2" href="https://www.kaspersky.com/resource-center/definitions/cookies" target='_blank'>
+                          Kaspersky  
+                        </a>
                     </p>
                 </div>
                 <div className="mt-14" id='what-data-we-collect'>
@@ -28,7 +37,14 @@ export default function () {
                         </a>
                     </h2>
                     <p className="mt-10">
-                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                        We collect the following data :
+                        <ul className="list-decimal ml-5">
+                            <li>Theme</li>
+                            <li>Page you visit</li>
+                            <li>Time you visit each page</li>
+                            <li>Which platform you visit from</li>
+                            <li>Which city and country you visit from</li>
+                        </ul>
                     </p>
                 </div>
                 <div className="mt-14" id='how-and-why-do-we-use-cookies'>
