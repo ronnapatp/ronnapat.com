d92eaba8f20c4ed4d1779e769a7462fac1efc9c1
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-10 16:45:45 +0700

    Update policy page

--- components/cookie.js
+++ components/cookie.js
@@ -1,7 +1,7 @@
 export default function () {
     return(
         <div className="relative container mx-auto bg-white px-4">
-            <div className="max-w-prose mx-auto py-8 prose-h1:md:text-6xl prose-h1:text-4xl prose-h1:font-semibold">
+            <div className="max-w-prose mx-auto py-8 prose-h1:md:text-6xl prose-h1:text-4xl prose-h1:font-semibold prose-p:text-gray-600 prose-h2:text-2xl prose-h2:md:text-4xl">
                 <h1>
                     Cookies Policy
                 </h1>
@@ -9,6 +9,72 @@ export default function () {
                     Last update : 10/21/12
                 </p>
                 <hr className="mt-5" />
+                <div className="mt-14" id='what-is-cookies'>
+                    <h2 className="tag inline-flex">
+                        What is cookies?
+                        <a href="#what-is-cookies" className="showtag px-2 bg-gray-200 text-gray-500 rounded-md ml-2">
+                            #
+                        </a>
+                    </h2>
+                    <p className="mt-10">
+                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                    </p>
+                </div>
+                <div className="mt-14" id='what-data-we-collect'>
+                    <h2 className="tag inline-flex">
+                        What data we collect?
+                        <a href="#what-data-we-collect" className="showtag px-2 bg-gray-200 text-gray-500 rounded-md ml-2">
+                            #
+                        </a>
+                    </h2>
+                    <p className="mt-10">
+                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                    </p>
+                </div>
+                <div className="mt-14" id='how-and-why-do-we-use-cookies'>
+                    <h2 className="tag inline-flex">
+                        How and Why do We Use Cookies?
+                        <a href="#how-and-why-do-we-use-cookies" className="showtag px-2 bg-gray-200 text-gray-500 rounded-md ml-2">
+                            #
+                        </a>
+                    </h2>
+                    <p className="mt-10">
+                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                    </p>
+                </div>
+                <div className="mt-14" id='how-can-i-block-cookie'>
+                    <h2 className="tag inline-flex">
+                        How can I block cookies?
+                        <a href="#how-can-i-block-cookie" className="showtag px-2 bg-gray-200 text-gray-500 rounded-md ml-2">
+                            #
+                        </a>
+                    </h2>
+                    <p className="mt-10">
+                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                    </p>
+                </div>
+                <div className="mt-14" id='changes-of-cookies-policy'>
+                    <h2 className="tag inline-flex">
+                        Changes of cookies policy?
+                        <a href="#changes-of-cookies-policy" className="showtag px-2 bg-gray-200 text-gray-500 rounded-md ml-2">
+                            #
+                        </a>
+                    </h2>
+                    <p className="mt-10">
+                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                    </p>
+                </div>
+                <div className="mt-14" id='contact-for-more-information'>
+                    <h2 className="tag inline-flex">
+                        Contact for more information
+                        <a href="#contact-for-more-information" className="showtag px-2 bg-gray-200 text-gray-500 rounded-md ml-2">
+                            #
+                        </a>
+                    </h2>
+                    <p className="mt-10">
+                        letmasiasd asotsadof dsfa sdf asd fasdfl;asd faklsd falksdjfalksdf asdf asd fasdl fasd falsd falsdkfalsd falsd flasjd flajs dflas dflasj dflka sdfsd fa sdlkf alsdflaksdfjklasdjfladsfasdlfk asdklfjalskdf asdfasd fa sdf asd fas df asdf asd fa sdf asdf asd fasd fasd fs df sad fa sdf asdf asd fas df asdf asd fa sdf asd fa sdf asdf asd fas df asdf asdf asd fasd f ewr we e wr qwer eqwr wqe xc vz xcv csxz xcb fds gasefr y rstju hj dfg hfv as r2345 23fasd fawe ar 34f dsva fsy df sgdf gse r f gsd ftgwerg dfsg sdf gadfs gsd fg aew r fag sd fasd fas df adsf asd f asd h rwe gacsxv aersgtsdf 
+                    </p>
+                </div>
             </div>
         </div>
     )
