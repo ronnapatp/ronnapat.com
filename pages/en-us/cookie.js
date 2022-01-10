c726f44c1afe4bab818e754b95afcb9f64b5af3e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-10 16:13:16 +0700

    Add navbar to cookie

--- pages/en-us/cookie.js
+++ pages/en-us/cookie.js
@@ -1,7 +1,12 @@
+import Navbar from "../../components/navbar"
 import Cookie from "../../components/cookie"
+import {
+    langenus
+} from '../../script/languages'
 export default function () {
     return(
         <>
+            <Navbar lantoshow={langenus} pathname='en-us' page='/cookie' />
             <Cookie />
         </>
     )
