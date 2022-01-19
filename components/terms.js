ff253b4b6d3040469cd876a3887cedccc8daa995
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-19 15:04:35 +0700

    Add terms

---
+++ components/terms.js
@@ -0,0 +1,7 @@
+export default function () {
+    return(
+        <div className="text-center text-3xl py-40">
+            This pages comming soon
+        </div>
+    )
+}
---
+++ pages/en-us/terms.js
@@ -0,0 +1,17 @@
+import Head from '../../components/meta'
+import Navbar from '../../components/navbar'
+import Terms from '../../components/terms'
+import Footer from '../../components/footer'
+import {
+    langenus
+} from '../../script/languages'
+export default function () {
+    return(
+        <>
+            <Head title='terms' image='/meta.png' />
+            <Navbar lantoshow={langenus} pathname='en-us' page='/terms' />
+            <Terms />
+            <Footer paht='en-us' />
+        </>
+    )
+}
