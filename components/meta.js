3c63166190e594f8f05cb256bfad6f783780738b
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-20 17:21:52 +0700

    Update meta

--- components/meta.js
+++ components/meta.js
@@ -8,17 +8,17 @@ export default function Meta({
     return(
         <Head>
             <title>
-                {title} - {Name}
+                {title} | {Name}
             </title>
             <meta property="og:url" content="https://ronnapat.com/" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content={`${title} - ${Name}`} />
+            <meta property="og:title" content={`${title} | ${Name}`} />
             <meta property="og:description" content={`${Name} personal website`} />
             <meta property="og:image" content={image} />
             <meta property="og:keywords" content="Ronnapat Srivoravilai" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@ronnapatp" />
-            <meta name="twitter:title" content={`${title} - ${Name}`} />
+            <meta name="twitter:title" content={`${title} | ${Name}`} />
             <meta name="twitter:description" content={`${Name} personal website`} />
             <meta name="twitter:creator" content="@ronnapatp" />
             <meta name="twitter:image:src" content={image} />
