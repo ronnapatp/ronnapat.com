9eabe64b04e587dc770b4c129f98938613ee041d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-25 11:13:41 +0700

    Update donate

--- components/donate.js
+++ components/donate.js
@@ -1,7 +1,10 @@
+import Script from "next/script"
+
 export default function () {
     return(
-        <div>
-            
+        <div className="">
+            <Script src="https://donorbox.org/widget.js" paypalExpress="false"></Script>
+            <iframe src="https://donorbox.org/embed/donate-to-ronnapat" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%"></iframe>
         </div>
     )
 }
--- pages/en-us/donate.js
+++ pages/en-us/donate.js
@@ -10,8 +10,6 @@ export default function () {
         <>
             <Head title='Privacy' image='/meta.png' />
             <Navbar lantoshow={langenus} pathname='en-us' page='/privacy' />
-            <Script src="https://donorbox.org/widget.js" paypalExpress="false"></Script>
-            <iframe src="https://donorbox.org/embed/donate-to-ronnapat" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%"></iframe>
             <Footer paht='en-us' />
         </>
     )
