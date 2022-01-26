43f3c73b259e6d44a46a186e1a931fda807f965f
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-26 20:57:11 +0700

    Update donate page

--- components/donate.js
+++ components/donate.js
@@ -3,6 +3,7 @@ import Container from "./container"
 export default function () {
     return(
         <div className="max-w-md mx-auto py-2 my-20 px-2 md:my-40">
+            Donate To ronnapat
             <Script src="https://donorbox.org/widget.js" paypalExpress="false"></Script>
             <iframe src="https://donorbox.org/embed/donate-to-ronnapat" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%"></iframe>
         </div>
