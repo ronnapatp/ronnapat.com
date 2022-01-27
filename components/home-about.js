c4e5832bb3f13513a4d163c8ae6abfa2f18f3d9e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-27 16:21:16 +0700

    Update skill and homepage

--- components/home-about.js
+++ components/home-about.js
@@ -1,16 +1,23 @@
+import Container from './container'
 export default function Homeabout({
     title,
-    children
+    children,
+    paht,
 }) {
     return(
         <>
-            <div className="text-center dark:bg-slate-800 py-16 sm:py-20 md:py-28 sm:px-4 lg:px-72 bg-white" id="about">
-                <h1 className="text-4xl md:text-5xl font-semibold dark:text-white">
-                    {title}
-                </h1>
-                <div className="text-xl md:text-3xl font-normal dark:text-slate-300">
-                    {children}
-                </div>
+            <div className="text-center py-20 dark:bg-slate-800 bg-white" id="about">
+                <Container>
+                    <h1 className="text-4xl md:text-6xl font-semibold dark:text-white">
+                        {title}
+                    </h1>
+                    <div className="text-xl md:text-2xl mt-5 mb-8 font-normal dark:text-slate-300">
+                        {children}
+                    </div>
+                    <a className="after:content-['_↗'] text-gray-600 dark:text-gray-200" href={`/${paht}/about`}>
+                        View all in about page
+                    </a>
+                </Container>
             </div>
         </>
     )
--- pages/en-us/index.js
+++ pages/en-us/index.js
@@ -4,7 +4,6 @@ import Head from '../../components/meta'
 import OtherHead from 'next/head'
 import Script from 'next/script'
 import Homeabout from "../../components/home-about";
-import Skill from "../../components/skill";
 import Project from '../../components/project'
 import Goblog from '../../components/go-blog'
 import Subscribe from "../../components/subscribe";
@@ -25,12 +24,9 @@ export default function Home() {
             </OtherHead>
             <Navbar lantoshow={langenus} pathname='en-us' page='/' />
             <Header name='Ronnapat Srivoravilai' description='I create website bot and much more! You can view my project in my blog' button='Learn more' bthref='#about' />
-            <Homeabout title='Who am I ?'>
+            <Homeabout title='Who am I ?' paht='en-us'>
                 I am a student in bangkok ,Thailand. I like to create website bot and application. I am currently working on bottlecrush app (View it on <a href="https://github.com/bottlecrush" className="hover:underline hover:text-sky-500 dark:hover:text-sky-400">github</a>)
             </Homeabout>
-            <center>
-                <Skill />
-            </center>
             <Project />
             <Goblog />
             <Subscribe />
