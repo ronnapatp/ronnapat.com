690d359ad6f8d5939b4417e01c1415b60d72cdbb
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-31 09:48:34 +0700

    Try google ads again

---
+++ components/ads.js
@@ -0,0 +1,18 @@
+import Script from 'next/script'
+export default function () {
+    return(
+        <>
+            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1196910141338252"
+     crossorigin="anonymous"></Script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1196910141338252"
+     data-ad-slot="2252611327"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<Script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</Script>
+        </>
+    )
+}
