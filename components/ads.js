f2ceed4918c23cf2018ee690a9fa820e9ccd7b41
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-01-31 10:12:24 +0700

    Try google ads again

--- components/ads.js
+++ components/ads.js
@@ -1,18 +1,20 @@
-import Script from 'next/script'
+import { useEffect } from "react";
 export default function () {
+    useEffect(() => {
+        var ads = document.getElementsByClassName("adsbygoogle").length;
+        for (var i = 0; i < ads; i++) {
+          try {
+            (adsbygoogle = window.adsbygoogle || []).push({});
+          } catch (e) { }
+        }
+  }, []);
     return(
         <>
-            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1196910141338252"
-     crossorigin="anonymous"></Script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1196910141338252"
-     data-ad-slot="2252611327"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<Script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</Script>
+        <ins className="adsbygoogle"
+            data-ad-client="ca-pub-1196910141338252"
+            data-ad-slot="2252611327"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
         </>
     )
 }
