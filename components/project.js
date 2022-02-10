bee4ca021c081c93c55fc4cb510e82d72955468d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-10 17:02:41 +0700

    Update project

--- components/project.js
+++ components/project.js
@@ -17,7 +17,7 @@ export default function () {
                         </a>
                     </div>
                     <div className="p-6 max-w-screen mt-3 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-2 md:mb-0 hover:scale-105 duration-100">
-                        <a href='#'>
+                        <a href='https://blog.ronnapat.com/blog/my-website' target='_blank'>
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">My website ronnapat.com</h5>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Since last year I create 2 version of my website and in this year I create version 1.2.0</p>
                         </a>
