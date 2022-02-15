6eff4e2d7e6664527aec31f78d9307467e619401
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-15 18:14:37 +0700

    Update footer

--- components/footer.js
+++ components/footer.js
@@ -120,6 +120,9 @@ export default function footer({
               <a href="https://github.com/ronnapatp/ronnapat.com" target='_blank' className="my-3 block text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-100 text-sm font-medium duration-700">
                   {repo}
               </a>
+              <a href="/donate" target='_blank' className="my-3 block text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-100 text-sm font-medium duration-700">
+                  Donate
+              </a>
               <button onClick={openModal} className="my-3 block text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-100 text-sm font-medium duration-700">
                   {share}
               </button>
