012000bf38cf0566f420c0ae48f0ae26245f4e02
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 17:30:11 +0700

    fix for eslintrc

--- components/project.js
+++ components/project.js
@@ -11,13 +11,13 @@ export default function () {
                         My project
                     </div>
                     <div className="p-6 max-w-screen mt-3 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-2 md:mb-0 hover:scale-105 duration-100">
-                        <a href='https://blog.ronnapat.com/blog/my-twitter-bot' target='_blank'>
+                        <a href='https://blog.ronnapat.com/blog/my-twitter-bot' target='_blank' rel="noopener">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">My twitter bot with typescript</h5>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">I build my first twitter bot to update my bio using twitter api with typescript</p>
                         </a>
                     </div>
                     <div className="p-6 max-w-screen mt-3 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-2 md:mb-0 hover:scale-105 duration-100">
-                        <a href='https://blog.ronnapat.com/blog/my-website' target='_blank'>
+                        <a href='https://blog.ronnapat.com/blog/my-website' target='_blank' rel="noopener">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">My website ronnapat.com</h5>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Since last year I create 2 version of my website and in this year I create version 1.2.0</p>
                         </a>
@@ -25,7 +25,7 @@ export default function () {
                 </div>
             </Container>
             <div className='text-center pb-10'>
-                <a className="after:content-['_↗'] text-gray-600 dark:text-gray-200" href='https://blog.ronnapat.com/tags/project' target='_blank'>
+                <a className="after:content-['_↗'] text-gray-600 dark:text-gray-200" href='https://blog.ronnapat.com/tags/project' target='_blank' rel="noopener">
                     View all in project blog
                 </a>
             </div>
