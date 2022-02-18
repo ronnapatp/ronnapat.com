eb4d428c370147e08137f395542a266ccb68f5c3
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 20:29:14 +0700

    Update input and footer

--- components/faq.js
+++ components/faq.js
@@ -28,7 +28,7 @@ export default function () {
           <Container>
           <input 
           type='text' 
-          placeholder="Search..." 
+          placeholder="Search... " 
           value={search} 
           onChange={(event)=> {setSearch(event.target.value)}}
           className="mt-3 rounded-md block shadow-sm border-gray-300 dark:bg-slate-600 dark:text-white dark:border-sky-400 dark:placeholder-gray-400 w-full focus:border-sky-300 focus:ring focus:ring-sky-200 focus:ring-opacity-50 mb-2"
--- components/footer.js
+++ components/footer.js
@@ -112,7 +112,7 @@ export default function footer({ paht }) {
             Terms of service
           </a>
           <a
-            href={`/${paht}/terms`}
+            href={`/${paht}/faq`}
             className="my-3 block text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-100 text-sm font-medium duration-700"
           >
             Frequently Asked Question
