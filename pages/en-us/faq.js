af8597bb5c1e0504a8a3ea24be6429f5ec034c4b
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 16:26:58 +0700

    Set up faq

--- pages/en-us/faq.js
+++ pages/en-us/faq.js
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar"
 import Head from "../../components/meta"
+import Faq from "../../components/faq"
 import Footer from "../../components/footer"
 import {
     langenus
@@ -9,7 +10,8 @@ export default function faq () {
         <>
             <Head title='FAQ' image='/meta.png' />
             <Navbar lantoshow={langenus} pathname='en-us' page='/faq' />
-            
+            <Faq />
+            <Footer />
         </>
     )
 }
