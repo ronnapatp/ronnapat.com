6aa79bba41d4cef90e9b67639dcca5c7b59dab95
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 16:37:44 +0700

    Update faq

--- components/faq.js
+++ components/faq.js
@@ -3,7 +3,10 @@ import { Collapse } from '@nextui-org/react';
 
 export default function (){
     return(
-        <div className="bg-white dark:bg-slate-700 text-black dark:text-white">
+        <div className="bg-white dark:bg-slate-700 text-black dark:text-white py-24">
+          <h1 className='text-center text-5xl'>
+            Ronnapat Srivoravilai FAQ
+          </h1>
           <Container>
             <Collapse.Group>
               <Collapse title={<h1 className='text-black dark:text-white text-3xl'>Option 1</h1>} >
