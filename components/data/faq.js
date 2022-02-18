9e0a93cdab4caa4dc6f5e9ed4c89a4b13cb6e561
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 16:56:20 +0700

    Update faq

--- components/data/faq.js
+++
@@ -1,5 +0,0 @@
-export const faq = [
-    { id: '1', title: 'Option 1', description: '<a href="#hello">Hello</a>'},
-    { id: '1', title: 'Option 1', description: '<a href="#hello">Hello</a>'},
-    { id: '1', title: 'Option 1', description: '<a href="#hello">Hello</a>'}
-]
