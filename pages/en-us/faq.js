6b8fab3c5cc13c6e74406d457df0fcded0456528
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 16:20:26 +0700

    Update and create faq page

---
+++ pages/en-us/faq.js
@@ -0,0 +1,15 @@
+import Navbar from "../../components/navbar"
+import Head from "../../components/meta"
+import Footer from "../../components/footer"
+import {
+    langenus
+} from '../../script/languages'
+export default function faq () {
+    return(
+        <>
+                        <Head title='Cookies' image='/meta.png' />
+            <Navbar lantoshow={langenus} pathname='en-us' page='/cookie' />
+            
+        </>
+    )
+}
