c2346f62cc939392dc22e79b9b56c798b18efbc3
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 16:56:13 +0700

    Update faq

---
+++ components/data/faq.js
@@ -0,0 +1,5 @@
+export const faq = [
+    { id: '1', title: 'Option 1', description: '<a href="#hello">Hello</a>'},
+    { id: '1', title: 'Option 1', description: '<a href="#hello">Hello</a>'},
+    { id: '1', title: 'Option 1', description: '<a href="#hello">Hello</a>'}
+]
--- components/faq.js
+++ components/faq.js
@@ -1,32 +1,28 @@
 import Container from './container'
 import { Collapse } from '@nextui-org/react';
 
+const faq = [
+  { id: '1', title: 'Option 1', description: 'Coming soon!'},
+  { id: '2', title: 'Option 1', description: 'Coming soon!'},
+  { id: '3', title: 'Option 1', description: 'Coming soon!'}
+]
+
 export default function (){
     return(
         <div className="bg-white dark:bg-slate-700 text-black dark:text-white py-24">
-          <h1 className='text-center text-5xl'>
+          <h1 className='text-center text-5xl mb-4'>
             Ronnapat Srivoravilai FAQ
+            <p className='text-xl'>
+              If you have more question you can contact me at <a href='/contact'>Contact page</a>
+            </p>
           </h1>
           <Container>
             <Collapse.Group>
-              <Collapse title={<h1 className='text-black dark:text-white text-3xl'>Option 1</h1>} >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+            {faq.map((faq) => (
+              <Collapse id={faq.id} title={<h1 className='text-black dark:text-white text-3xl'>{faq.title}</h1>} >
+                {faq.description}
               </Collapse>
-              <Collapse title={<h1 className='text-black dark:text-white text-3xl'>Option 1</h1>} >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
-              </Collapse>
-              <Collapse title={<h1 className='text-black dark:text-white text-3xl'>Option 1</h1>} >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
-              </Collapse>
+            ))}
             </Collapse.Group>
           </Container>
         </div>
