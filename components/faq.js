8f4c43608b9dec7ad1f3bb6661e01a5b3264abc2
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 20:22:47 +0700

    Add search bar

--- components/faq.js
+++ components/faq.js
@@ -1,16 +1,23 @@
 import Container from "./container";
 import { Collapse } from "@nextui-org/react";
+import { useState } from "react";
 
 const faq = [
   { id: "1", title: "Option 1", description: "Coming soon!" },
   { id: "2", title: "Option 1", description: "Coming soon!" },
   { id: "3", title: "Option 1", description: "Coming soon!" },
-  { id: "4", title: "Option 1", description: "Coming soon!" },
+  { id: "4", title: "s", description: "Coming soon!" },
   { id: "5", title: "Option 1", description: "Coming soon!" },
   { id: "6", title: "Option 1", description: "Coming soon!" },
 ];
 
 export default function () {
+  const [ search, setSearch ] = useState('')
+
+  const filtersearch = faq.filter((faq) => {
+    return faq.title.includes(search);
+  })
+  
   return (
     <div className="bg-white dark:bg-slate-700 text-black dark:text-white py-24">
       <h1 className="text-center text-5xl mb-4">
@@ -18,11 +25,12 @@ export default function () {
         <p className="text-xl">
           If you have more question you can contact me at{" "}
           <a href="/contact">Contact page</a>
+          <input type='text' placeholder="text" value={search} onChange={(event)=> {setSearch(event.target.value)}} />
         </p>
       </h1>
       <Container>
         <Collapse.Group>
-          {faq.map((faq) => (
+          {filtersearch.map((faq) => (
             <Collapse
               key={faq.id}
               title={
