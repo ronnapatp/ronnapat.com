51111938ec436a88b3c444b9e032220433ff9082
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-21 16:49:24 +0700

    Update navbar

--- components/navbar.js
+++ components/navbar.js
@@ -13,9 +13,6 @@ import {
   TranslateIcon,
   CashIcon,
   LinkIcon,
-  BookmarkAltIcon,
-  CalendarIcon,
-  ShieldCheckIcon,
 } from "@heroicons/react/outline";
 
 const pageshover = [
@@ -225,7 +222,16 @@ export default function Example({ lantoshow, pathname, page }) {
                       'group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 dark:hover:text-gray-400'
                     )}
                   >
-                    <span>More</span>
+                    <span>
+                      More
+                    </span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-900 dark:text-white' : 'text-gray-500 dark:text-gray-200',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
                   </Popover.Button>
 
                   <Transition
@@ -237,7 +243,7 @@ export default function Example({ lantoshow, pathname, page }) {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 translate-y-1"
                   >
-                    <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
+                    <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
                       <div className="rounded-lg shadow-lg ring-2 ring-black dark:ring-slate-700 ring-opacity-5 overflow-hidden">
                         <div className="relative grid gap-6 bg-gray-50 dark:bg-slate-800 px-5 py-6 sm:gap-8 sm:p-8">
                           {resources.map((item) => (
