4051ce91958f72dd1ffecce8d35448b9de68d689
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-21 18:13:36 +0700

    Delete buymeacoffee

--- pages/_app.js
+++ pages/_app.js
@@ -48,18 +48,6 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
         <Cookies />
       </div>
-      <script
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-id="ronnapatp"
-        data-description="Support me on Buy me a coffee!"
-        data-message="Donate to ronnapatp"
-        data-color="#5F7FFF"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"
-      ></script>
     </ThemeProvider>
   );
 }
