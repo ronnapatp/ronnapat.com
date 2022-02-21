36a5192ae70bf13e66f21662b2223bf90342f00f
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-21 18:08:24 +0700

    Update donate

---
+++ pages/en-us/donate.js
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar";
+import Head from "@/components/meta";
+import Donate from "@/components/donate";
+import Footer from "@/components/footer";
+import { langenus } from "@/script/languages";
+
+export default function () {
+  return (
+    <>
+      <Navbar lantoshow={langenus} pathname="en-us" page="/donate" />
+      <Head title="Donate" image="/meta.png" />
+      <Donate />
+      <Footer paht="en-us" />
+    </>
+  );
+}
