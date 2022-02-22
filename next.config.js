a4921813b89942fa11340fa15bda5aaa2163fa9c
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-22 19:13:06 +0700

    Update to deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false, process: false };
+    config.resolve.fallback = { fs: false, path: false, process: false, buffer: false };
 
     return config;
   },
