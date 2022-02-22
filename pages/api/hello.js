832a109fa2a1cd4324db14240a2753c5bae69c5e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-22 19:11:55 +0700

    Manage code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default function helloAPI(req, res) {
-  res.status(200).json({ 
+  res.status(200).json({
     name: "John Doe",
-    age: 23
+    age: 23,
   });
 }
