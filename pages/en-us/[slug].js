8d1b9d7698cfa24845f8d5f0cd82a3d7c2590696
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-22 22:36:26 +0700

    Update terms

--- pages/en-us/[slug].js
+++ pages/en-us/[slug].js
@@ -1,34 +1,72 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import md from 'markdown-it';
+import Container from '@/components/container'
+import { MDXRemote } from 'next-mdx-remote';
+import Head from 'next/head';
+const CustomLink = [`
+<Link as={as} href={href}>
+<a {...otherProps} />
+</Link>`]
+import {
+  getPostBySlug,
+  postFilePaths,
+} from '@/script/mdx-utils';
+i
 
-export async function getStaticPaths() {
-  const files = fs.readdirSync('page');
-  const paths = files.map((fileName) => ({
-    params: {
-      slug: fileName.replace('.mdx', ''),
+// Custom components/renderers to pass to MDX.
+// Since the MDX files aren't loaded by webpack, they have no knowledge of how
+// to handle import statements. Instead, you must include components in scope
+// here.
+const components = {
+  a: CustomLink,
+  // It also works with dynamically-imported components, which is especially
+  // useful for conditionally loading components for certain routes.
+  // See the notes in README.md for more details.
+  Head,
+};
+
+export default function PostPage({
+  source,
+  frontMatter,
+  globalData,
+}) {
+  return (
+    <div className='bg-white dark:bg-slate-700 w-full'>
+      <Container>
+        {/* <header>
+          <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
+            {frontMatter.title}
+          </h1>
+        </header> */}
+        <main className='max-w-prose mx-auto py-8 prose-h1:font-semibold dark:prose-h1:text-white prose-p:text-gray-600 dark:prose-p:text-gray-300 prose-h2:text-2xl prose-h2:md:text-4xl dark:prose-a:text-gray-100 prose-a:text-gray-600  hover:prose-a:text-sky-500 dark:hover:prose-a:text-sky-300 hover:prose-a:underline'>
+          <h1>
+          {frontMatter.title}
+            </h1>
+          <MDXRemote {...source} components={components} />
+        </main>
+      </Container>
+    </div>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { mdxSource, data } = await getPostBySlug(params.slug);
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
     },
-  }));
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = postFilePaths
+    // Remove file extensions for page paths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    // Map the path into the static paths object required by Next.js
+    .map((slug) => ({ params: { slug } }));
+
   return {
     paths,
     fallback: false,
   };
-}
-
-export async function getStaticProps({ params: { slug } }) {
-  const fileName = fs.readFileSync(`page/${slug}.mdx`, 'utf-8');
-  const { content } = matter(fileName);
-  return {
-    props: {
-      content,
-    },
-  };
-}
-
-export default function PostPage({ frontmatter, content }) {
-  return (
-    <div className='prose mx-auto'>
-      <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
-    </div>
-  );
-}
+};
