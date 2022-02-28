f08485dc87e94f52fd21df05eb8bd0ba03ae8d8d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-28 18:27:37 +0700

    Update script

--- pages/_app.js
+++ pages/_app.js
@@ -39,12 +39,12 @@ function MyApp({ Component, pageProps }) {
             `,
         }}
       />
-      <script
+      <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1196910141338252"
         crossOrigin="anonymous"
       />
-      <script
+      <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1196910141338252"
         crossOrigin="anonymous"
