13e43175c00147b349d34ae146eabb96b5090b95
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-02 13:25:45 +0700

    Update css

--- pages/en-us/[slug].js
+++ pages/en-us/[slug].js
@@ -29,7 +29,7 @@ export default function PostPage({ source, frontMatter }) {
     <div className="bg-white dark:bg-slate-700">
       <Navbar />
       <Container>
-      <main className="max-w-prose mx-auto py-8 prose-h1:font-semibold dark:prose-h1:text-white prose-p:text-gray-600 dark:prose-p:text-gray-300 prose-h2:text-2xl prose-h2:md:text-4xl dark:prose-a:text-gray-100 prose-a:text-gray-600  hover:prose-a:text-sky-500 dark:hover:prose-a:text-sky-300 hover:prose-a:underline">
+      <main className="max-w-prose mx-auto py-8 prose-h1:font-semibold dark:prose-h1:text-white prose-p:text-gray-600 dark:prose-p:text-gray-300 prose-h2:text-2xl prose-h2:md:text-4xl dark:prose-a:text-gray-100 prose-a:text-gray-600  hover:prose-a:text-sky-500 dark:hover:prose-a:text-sky-300 hover:prose-a:underline prose-h3:text-xl prose-h3:md:text-2xl">
           <h1 className="md:text-6xl text-4xl">{frontMatter.title}</h1>
           <p>Last update : {frontMatter.update}</p>
           <hr className="mt-5" />
