a57a0b1d26ef6a3be1bff3f41d728082491d62ed
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-03 12:34:03 +0700

    Update blog

--- script/mdxUtils.js
+++ script/mdxUtils.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), "page/blog");
+export const POSTS_PATH = path.join(process.cwd(), "data/posts");
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
