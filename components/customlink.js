f6d1ae943b4a2a4c45abdd2da4265992f4222c35
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-04 08:54:10 +0700

    Fix about prose

--- components/customlink.js
+++ components/customlink.js
@@ -4,7 +4,7 @@ export default function CustomLink({ as, href, ...otherProps }) {
   return (
     <>
       <Link as={as} href={href}>
-        <a {...otherProps} className="text-black hover:text-sky-400 dark:text-white dark:hover:text-sky-300" />
+        <a {...otherProps} className="no-underline text-black hover:text-sky-400 hover:underline dark:text-white dark:hover:text-sky-300" />
       </Link>
     </>
   );
