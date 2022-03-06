40ce3f9a5619ad4ab0cfb88c7cbc8fbeee16a1c8
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-06 15:06:15 +0700

    Update layout

--- components/layout/mdx.js
+++ components/layout/mdx.js
@@ -1,3 +1,3 @@
 export default function ({children}) {
-    return(<div className="prose prose-slate prose-p:text-gray-600 dark:prose-p:text-gray-300 prose-h3:text-black dark:prose-h3:text-white">{children}</div>)
+    return(<div className="prose-layout">{children}</div>)
 }
