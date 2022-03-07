e474877ae52ea2e557f5e594f5e7a9ffced95441
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-07 12:28:52 +0700

    Update brand

--- components/brand/logo.js
+++ components/brand/logo.js
@@ -34,6 +34,29 @@ export default function () {
           <textarea className="text-gray-700 resize-none rounded-md cursor-text	w-full select-all" disabled>https://file.ronnapat.me/logo-no-bg.zip</textarea>
         </div>
       </div>
+      <div class="block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <h2 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white text-center">
+          Other image
+        </h2>
+        <h2 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white text-center">
+          Other image
+        </h2>
+        <h2 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white text-center">
+          Other image
+        </h2>
+        <h2 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white text-center">
+          Other image
+        </h2>
+        <p class="font-normal text-gray-700 dark:text-gray-200 text-center bg-gray-200 dark:bg-slate-600  ">
+          <a href="https://file.ronnapat.me/other-image.zip" target="_blank" className="inline-flex">
+              Download
+              <DownloadIcon className="w-6 h-6" />
+          </a>
+        </p>
+        <div className="px-3">
+          <textarea className="text-gray-700 resize-none rounded-md cursor-text	w-full select-all" disabled>https://file.ronnapat.me/other-image.zip</textarea>
+        </div>
+      </div>
     </div>
   );
 }
