a4ef54c24cb3dfac581b13da3872f0dccce8eb66
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-09 11:14:47 +0700

    Update auth

--- pages/en-us/login.js
+++ pages/en-us/login.js
@@ -56,7 +56,7 @@ export default function Register() {
     const actionCodeSettings = {
         // URL you want to redirect back to. The domain (www.example.com) for this
         // URL must be in the authorized domains list in the Firebase Console.
-        url: 'http://locahost:3000',
+        url: 'http://ronnapat.com/',
         // This must be true.
         handleCodeInApp: true,
       };
