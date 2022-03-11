8c97c759bdb9115911476245a91bebad379561f2
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-11 16:42:40 +0700

    Update auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 import TwitterProvider from "next-auth/providers/twitter"
+import EmailProvider from "next-auth/providers/email"
 
 export default NextAuth({
   providers: [
@@ -12,5 +13,21 @@ export default NextAuth({
       clientId: process.env.NEXT_PUBLIC_TWITTER_ID,
       clientSecret: process.env.NEXT_PUBLIC_TWITTER_SECRET,
     }),
-  ]
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    }),
+  ],
+  database: {
+    type: "sqlite",
+    database: ":memory:",
+    synchronize: true,
+  },
 })
