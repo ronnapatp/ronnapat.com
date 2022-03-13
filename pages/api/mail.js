d05a80666513b2ca7fbb72dd53ada5d5067930bc
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-13 12:44:08 +0700

    Update mail

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,8 +3,6 @@ import sendgrid from "@sendgrid/mail";
 sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
-  // NOTE: Uncomment the below lines to make the code work
-
   try {
     await sendgrid.send({
       to: req.body.email, // Your email where you'll receive emails
@@ -14,7 +12,7 @@ async function sendEmail(req, res) {
         <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
               </div>
               <p><h2>Thanks for contacting Ronnapat Srivoravilai!</h2> <br /> we just receive you contact form and we will contact you back soon but if it pass 3 days and you didn't get any email back please contact again your form may lost </p>
-              <small class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards,<br>Ronnapat Srivoravilai<br>Visit website : <a href="https://ronnapat.com" target="_blank">ronnapat.com</a><br>Support : <a href="mailto:support@ronnapat.me">support@ronnapat.me</a><br><br><small style="color: #808388;">This is auto mail from ronnapat.com please don't reply this email</small></small>
+              <small class="footer" style="font-size: 7px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards,<br>Ronnapat Srivoravilai<br>Visit website : <a href="https://ronnapat.com" target="_blank">ronnapat.com</a><br>Support : <a href="mailto:support@ronnapat.me">support@ronnapat.me</a><br><br><small style="color: #808388;">This is auto mail from ronnapat.com please don't reply this email</small></small>
               </div>
       `,
     });
