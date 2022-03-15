a0e40952d5232fc834cacf7f336908792cfc5cef
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-15 16:23:54 +0700

    Update blog

--- components/layout/MarkdownBlog.js
+++ components/layout/MarkdownBlog.js
@@ -4,16 +4,20 @@ import Footer from "@/components/footer/footer";
 import Share from "./share"
 import { langenus } from "@/script/languages";
 import Container from "./container";
-export default function ({ children, title, page }) {
+export default function ({ children, title, page, date, author }) {
   return (
     <div className="bg-white dark:bg-slate-700">
         <Navbar lantoshow={langenus} pathname="en-us" page={`/blog/posts/${page}`} />
         <Head title={title} />
       <Container>
-        <div className="prose-layout">
-            <h1 className="text-center uppercase">
+            <h1 className="text-center uppercase text-2xl md:text-4xl font-semibold mt-10">
+          <span className="text-center text-sm md:text-xl font-normal">
+              {date} - <a href="#">@{author}</a> 
+              <br />
+          </span>
                 {title}
             </h1>
+        <div className="prose-layout">
             {children}
             <Share href={page} title={title} />
         </div>
