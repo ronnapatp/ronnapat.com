174e11a79d00640c0ee8b584dc220e9a704e0202
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-17 20:20:37 +0700

    Manage code

--- components/layout/MarkdownBlog.js
+++ components/layout/MarkdownBlog.js
@@ -1,28 +1,43 @@
-import Head from "@/components/global/meta"
-import Navbar from "@/components/navbar/navbar"
+import Head from "@/components/global/meta";
+import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
-import Share from "./share"
+import Share from "./share";
 import { langenus } from "@/script/languages";
 import Container from "./container";
-export default function ({ children, title, page, date, author, image, github }) {
+export default function ({
+  children,
+  title,
+  page,
+  date,
+  author,
+  image,
+  github,
+}) {
   return (
     <div className="bg-white dark:bg-slate-700">
-        <Navbar lantoshow={langenus} pathname="en-us" page={`/blog/posts/${page}`} />
-        <Head title={title} image={image} />
+      <Navbar
+        lantoshow={langenus}
+        pathname="en-us"
+        page={`/blog/posts/${page}`}
+      />
+      <Head title={title} image={image} />
       <Container>
-            <h1 className="text-center uppercase text-2xl md:text-4xl font-semibold mt-10">
+        <h1 className="text-center uppercase text-2xl md:text-4xl font-semibold mt-10">
           <span className="text-center text-sm md:text-xl font-normal">
-              {date} - <a href="/social">@{author}</a> 
-              <br />
+            {date} - <a href="/social">@{author}</a>
+            <br />
           </span>
-                {title}
-            </h1>
+          {title}
+        </h1>
         <div className="prose-layout">
-            {children}
-            <Share href={page} title={title} />
-            <p>
-                Have an issue? <a href={github} target="_blank">Fix it on Github.</a>
-            </p>
+          {children}
+          <Share href={page} title={title} />
+          <p>
+            Have an issue?{" "}
+            <a href={github} target="_blank">
+              Fix it on Github.
+            </a>
+          </p>
         </div>
       </Container>
       <Footer path="en-us" />
--- components/layout/share.js
+++ components/layout/share.js
@@ -5,7 +5,9 @@ export default function ({ href, title }) {
     <div>
       <div className="py-5 text-xl">
         Share : &nbsp;
-        <Link href={`https://www.facebook.com/sharer/sharer.php?u=https://ronnapat.com/en-us/blog/posts/${href}`}>
+        <Link
+          href={`https://www.facebook.com/sharer/sharer.php?u=https://ronnapat.com/en-us/blog/posts/${href}`}
+        >
           <a target="_blank">
             <div className="bg-[#1877f2] px-2 py-2 rounded-md text-2xl inline-flex text-white">
               <i className="bi bi-facebook"></i>
@@ -13,7 +15,9 @@ export default function ({ href, title }) {
           </a>
         </Link>
         &nbsp;
-        <Link href={`https://twitter.com/intent/tweet?url=https://ronnapat.com/en-us/blog/posts/${href}%20via%20@ronnapatp&text=${title}`}>
+        <Link
+          href={`https://twitter.com/intent/tweet?url=https://ronnapat.com/en-us/blog/posts/${href}%20via%20@ronnapatp&text=${title}`}
+        >
           <a target="_blank">
             <div className="bg-[#1DA1F2] px-2 py-2 rounded-md text-2xl inline-flex text-white">
               <i className="bi bi-twitter"></i>
