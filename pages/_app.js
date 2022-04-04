6fbb5a8e0db2c957faac53cd01541a7e97b6cace
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-04 16:43:34 +0700

    Add ads

--- pages/_app.js
+++ pages/_app.js
@@ -40,6 +40,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           `,
         }}
       />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1196910141338252"
+     crossorigin="anonymous"></script>
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
