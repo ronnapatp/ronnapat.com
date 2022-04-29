bade8e0d4ea00c8c9457cab5df997000dded9ae3
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-29 12:27:39 +0700

    update blog

--- src/pages/blog/post/[slug].js
+++ src/pages/blog/post/[slug].js
@@ -1,3 +1,4 @@
+import Layout from "@/components/layout/layout";
 import fs from "fs";
 import matter from "gray-matter";
 import md from "markdown-it";
@@ -30,9 +31,26 @@ export async function getStaticProps({ params: { slug } }) {
 
 export default function PostPage({ frontmatter, content }) {
   return (
-    <div className="prose mx-auto">
+        <Layout>
+    <div className="prose-layout">
       <h1>{frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="ronnapatp/ronnapat.com"
+        data-repo-id="R_kgDOGOMxZg"
+        data-category="Q&A"
+        data-category-id="DIC_kwDOGOMxZs4CO13q"
+        data-mapping="url"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async
+      ></script>
     </div>
+        </Layout>
   );
 }
