5127db435263d9e1776b74cf77f6c60c5766ca7d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-29 12:03:41 +0700

    add comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,20 @@ export default function Home() {
         about me at about page
       </div>
       <Project />
+      <script src="https://giscus.app/client.js"
+        data-repo="ronnapatp/ronnapat.com"
+        data-repo-id="R_kgDOGOMxZg"
+        data-category="Q&A"
+        data-category-id="DIC_kwDOGOMxZs4CO13q"
+        data-mapping="url"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script>
     </Layout>
   );
 }
