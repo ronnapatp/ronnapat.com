6c2a9e343bc7fcfa4065a422b05c04df76accafe
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-29 13:15:20 +0700

    update layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -5,7 +5,7 @@ export default function ({ children }) {
   return (
     <>
       <Navbar />
-      <div className="bg-white dark:bg-slate-700 text-black dark:text-white h-screen">
+      <div className="bg-white dark:bg-slate-700 text-black dark:text-white h-[100%]">
         <Container>
           {children}
           <Footer />
