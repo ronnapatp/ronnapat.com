551d06132903421c52245f27147b9722ebdcddcf
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-05-31 20:03:44 +0700

    Update footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -13,7 +13,7 @@ export default function footer({ paht }) {
         <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
           <li></li>
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
+            <a href="/privacy" class="mr-4 hover:underline md:mr-6">
               Privacy
             </a>
           </li>
